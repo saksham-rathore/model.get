3a951b410a24b40fb7ac0b255941fb4691cea73f
sammy <samrathore07062007@gmail.com>
2026-09-23 23:34:24 +0530

    feat: migrate database adapter from libsql to postgresql and add db package setup

--- packages/db/index.ts
+++ packages/db/index.ts
@@ -1,4 +1,7 @@
+import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "@prisma/client";
 
-export const prisma = new PrismaClient();
+const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
+
+export const prisma = new PrismaClient({ adapter });
 export * from "@prisma/client";
