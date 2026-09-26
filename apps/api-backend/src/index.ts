7ab9203ddcd166e39f0f8a72942863f45cd53feb
sammy <samrathore07062007@gmail.com>
2026-09-26 20:48:21 +0530

    feat: remove turborepo and add elysia api backend with chat completions endpoint

--- apps/api-backend/src/index.ts
+++ apps/api-backend/src/index.ts
@@ -1,14 +1,37 @@
+import bearer from "@elysiajs/bearer";
 import { Elysia } from "elysia";
 import { t } from "elysia";
 
-const app = new Elysia().post("/conversation", () => {
-
-}, {
-  body: t.Object({
-
-  })
-}).listen(3000);
+const app = new Elysia()
+.use(bearer())
+  .post(
+    "/api/v1/chat/completions",
+    ({ bearer, body }) => {
+      console.log(bearer);
+      console.log(body);
+      return "hii";
+    },
+    {
+      headers: t.Object({
+        Authorization: t.String(),
+        ContentType: t.Literal("application/json"),
+      }),
+      body: t.Object({
+        models: t.String(),
+        messages: t.Array(
+          t.Object({
+            role: t.Enum({
+              user: "user",
+              assistant: "assistant",
+            }),
+            content: t.String(),
+          }),
+        ),
+      }),
+    },
+  )
+  .listen(4000);
 
 console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
 );
